379c13cfb9330e43cc58f2c056060eaa36808ddc
Julien Delsescaux <jdlxt@debian12>
2025-02-18 12:22:03 +0100

    unit test + code cleaning + better readme handling

---
+++ cache/phpstan/cache/PHPStan/ff/3b/ff3b69874061ee9322465b6e93cc6706c6bf50ac.php
@@ -0,0 +1,234 @@
+<?php declare(strict_types = 1);
+
+// odsl-/home/jdlxt/__dev/repo/github-tools/php
+return \PHPStan\Cache\CacheItem::__set_state(array(
+   'variableKey' => 'v1',
+   'data' => 
+  array (
+    '/home/jdlxt/__dev/repo/github-tools/php/update-main-readme.php' => 
+    array (
+      0 => '9918ce9e28f73b23d179a6297347f88f8ac77098',
+      1 => 
+      array (
+      ),
+      2 => 
+      array (
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/jdlxt/__dev/repo/github-tools/php/src/GithubClient.php' => 
+    array (
+      0 => '7aca3ed3dead15c9aae00eeb059c696c12679dc9',
+      1 => 
+      array (
+        0 => 'deljdlx\\github\\githubclient',
+      ),
+      2 => 
+      array (
+        0 => 'deljdlx\\github\\__construct',
+        1 => 'deljdlx\\github\\clone',
+        2 => 'deljdlx\\github\\gettoken',
+        3 => 'deljdlx\\github\\getrepository',
+        4 => 'deljdlx\\github\\getuserrepositories',
+        5 => 'deljdlx\\github\\getownrepositories',
+        6 => 'deljdlx\\github\\hasnextpage',
+        7 => 'deljdlx\\github\\getnextpageurl',
+        8 => 'deljdlx\\github\\getlastresponseheaders',
+        9 => 'deljdlx\\github\\fetchgithubapi',
+        10 => 'deljdlx\\github\\parseheaders',
+        11 => 'deljdlx\\github\\buildrepositoriesresponse',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/jdlxt/__dev/repo/github-tools/php/src/Readme.php' => 
+    array (
+      0 => '733ca9d7b3029416fa4dd1f1868646e952ca388a',
+      1 => 
+      array (
+        0 => 'deljdlx\\github\\readme',
+      ),
+      2 => 
+      array (
+        0 => 'deljdlx\\github\\__construct',
+        1 => 'deljdlx\\github\\write',
+        2 => 'deljdlx\\github\\getdemourl',
+        3 => 'deljdlx\\github\\gettitle',
+        4 => 'deljdlx\\github\\getcontent',
+        5 => 'deljdlx\\github\\appendtopart',
+        6 => 'deljdlx\\github\\appendparttopart',
+        7 => 'deljdlx\\github\\deletepart',
+        8 => 'deljdlx\\github\\clearpart',
+        9 => 'deljdlx\\github\\replacepartcontent',
+        10 => 'deljdlx\\github\\partexists',
+        11 => 'deljdlx\\github\\getpart',
+        12 => 'deljdlx\\github\\compile',
+        13 => 'deljdlx\\github\\parse',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/jdlxt/__dev/repo/github-tools/php/src/Cache.php' => 
+    array (
+      0 => '3ea024eb4506f269d791aea899b562810002e684',
+      1 => 
+      array (
+        0 => 'deljdlx\\github\\cache',
+      ),
+      2 => 
+      array (
+        0 => 'deljdlx\\github\\__construct',
+        1 => 'deljdlx\\github\\get',
+        2 => 'deljdlx\\github\\set',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/jdlxt/__dev/repo/github-tools/php/src/Repository.php' => 
+    array (
+      0 => '4df00269eade82d138da83d7e7dd61937b255c78',
+      1 => 
+      array (
+        0 => 'deljdlx\\github\\repository',
+      ),
+      2 => 
+      array (
+        0 => 'deljdlx\\github\\__construct',
+        1 => 'deljdlx\\github\\getname',
+        2 => 'deljdlx\\github\\getfullname',
+        3 => 'deljdlx\\github\\getslug',
+        4 => 'deljdlx\\github\\geturl',
+        5 => 'deljdlx\\github\\isarchived',
+        6 => 'deljdlx\\github\\isprivate',
+        7 => 'deljdlx\\github\\getmainbranch',
+        8 => 'deljdlx\\github\\getreadme',
+        9 => 'deljdlx\\github\\getdemourl',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/jdlxt/__dev/repo/github-tools/php/src/RepositoryManager.php' => 
+    array (
+      0 => 'd2252ee90a5837f444e69ef7c4b5b5695caa5e5f',
+      1 => 
+      array (
+        0 => 'deljdlx\\github\\repositorymanager',
+      ),
+      2 => 
+      array (
+        0 => 'deljdlx\\github\\__construct',
+        1 => 'deljdlx\\github\\clone',
+        2 => 'deljdlx\\github\\add',
+        3 => 'deljdlx\\github\\commit',
+        4 => 'deljdlx\\github\\push',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/jdlxt/__dev/repo/github-tools/php/src/Interfaces/Cache.php' => 
+    array (
+      0 => '131e376b863c482840062d768e666fdf9d4fd202',
+      1 => 
+      array (
+        0 => 'deljdlx\\github\\interfaces\\cache',
+      ),
+      2 => 
+      array (
+        0 => 'deljdlx\\github\\interfaces\\get',
+        1 => 'deljdlx\\github\\interfaces\\set',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/jdlxt/__dev/repo/github-tools/php/src/Interfaces/Repository.php' => 
+    array (
+      0 => 'b08650e9ae556d34787ba905fb2b415d0ab8d664',
+      1 => 
+      array (
+        0 => 'deljdlx\\github\\interfaces\\repository',
+      ),
+      2 => 
+      array (
+        0 => 'deljdlx\\github\\interfaces\\getname',
+        1 => 'deljdlx\\github\\interfaces\\getfullname',
+        2 => 'deljdlx\\github\\interfaces\\getslug',
+        3 => 'deljdlx\\github\\interfaces\\geturl',
+        4 => 'deljdlx\\github\\interfaces\\isarchived',
+        5 => 'deljdlx\\github\\interfaces\\isprivate',
+        6 => 'deljdlx\\github\\interfaces\\getmainbranch',
+        7 => 'deljdlx\\github\\interfaces\\getreadme',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/jdlxt/__dev/repo/github-tools/php/tests/PhpUnitReadyTest.php' => 
+    array (
+      0 => 'fd5f973fc225c6cc1579a3ca0a8b55e562ce9c9c',
+      1 => 
+      array (
+        0 => 'phpunitreadytest',
+      ),
+      2 => 
+      array (
+        0 => 'testaddition',
+        1 => 'teststringmanipulation',
+        2 => 'testarrayoperations',
+        3 => 'testobjectinstance',
+        4 => 'testexceptionhandling',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/jdlxt/__dev/repo/github-tools/php/tests/GithubClientTest.php' => 
+    array (
+      0 => 'd1242ec74c3bed0d037f442b64e1e8049fe1ce9c',
+      1 => 
+      array (
+        0 => 'githubclienttest',
+      ),
+      2 => 
+      array (
+        0 => 'testgettoken',
+        1 => 'testgetownrepositories',
+        2 => 'getmockclient',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+    '/home/jdlxt/__dev/repo/github-tools/php/tests/ReadmeTest.php' => 
+    array (
+      0 => 'e0293a41e82807a3dea0417e8ca371352031d22e',
+      1 => 
+      array (
+        0 => 'readmetest',
+      ),
+      2 => 
+      array (
+        0 => 'testgettitle',
+        1 => 'testpartexists',
+        2 => 'testpartdoesnotexist',
+        3 => 'testgetpart',
+        4 => 'testappendtopart',
+        5 => 'testreplacepartcontent',
+        6 => 'testappendparttopart',
+        7 => 'testclearpart',
+        8 => 'testdeletepart',
+        9 => 'getreadme',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+  ),
+));
